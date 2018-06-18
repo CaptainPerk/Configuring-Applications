13c80e350b3ad0c1628c8d683c7800568a531aef
Jon Perkins <captainperk81@gmail.com>
2018-06-18 18:32:29 -0400

    Digging into the Configuration Detail

--- ConfiguringApplications/Program.cs
+++ ConfiguringApplications/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Hosting;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace ConfiguringApplications
 {
@@ -17,9 +10,14 @@ namespace ConfiguringApplications
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+        }
     }
 }
