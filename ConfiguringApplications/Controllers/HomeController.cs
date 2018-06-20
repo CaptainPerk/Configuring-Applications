645cc84603363a00c99e1eecc4b442ed57338c47
Jon Perkins <captainperk81@gmail.com>
2018-06-19 21:29:19 -0400

    Understanding ASP.NET Services

--- ConfiguringApplications/Controllers/HomeController.cs
+++ ConfiguringApplications/Controllers/HomeController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConfiguringApplications.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
 namespace ConfiguringApplications.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() => View(new Dictionary<string, string>() { ["Message"] = "This is the Index Action" });
-}
+        private readonly UptimeService _uptimeService;
+
+        public HomeController(UptimeService uptimeService)
+        {
+            _uptimeService = uptimeService;
+        }
+
+        public ViewResult Index() => View(new Dictionary<string, string>()
+        {
+            ["Message"] = "This is the Index Action",
+            ["Uptime"] = $"{_uptimeService.Uptime}ms"
+        });
+    }
 }
