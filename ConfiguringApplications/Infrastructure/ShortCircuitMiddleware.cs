672defdafa5b06c9a1b65c5c30d673863e97b2c5
Jon Perkins <captainperk81@gmail.com>
2018-06-20 07:20:10 -0400

    Creating Short Curcuiting Middleware

---
+++ ConfiguringApplications/Infrastructure/ShortCircuitMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfiguringApplications.Infrastructure
+{
+    public class ShortCircuitMiddleware
+    {
+        private readonly RequestDelegate _nextDelegate;
+
+        public ShortCircuitMiddleware(RequestDelegate nextDelegate)
+        {
+            _nextDelegate = nextDelegate;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Headers["User-Agent"].Any(h => h.ToLower().Contains("edge")))
+            {
+                httpContext.Response.StatusCode = 403;
+            }
+            else
+            {
+                await _nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+}
