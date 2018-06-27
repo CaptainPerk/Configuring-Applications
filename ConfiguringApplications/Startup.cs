eb8d4a4d9e146863d75b97d7c7625aded5417777
Jon Perkins <captainperk81@gmail.com>
2018-06-27 07:13:30 -0400

    Understanding How the Configure Method is Invoked

--- ConfiguringApplications/Startup.cs
+++ ConfiguringApplications/Startup.cs
@@ -15,10 +15,15 @@ namespace ConfiguringApplications
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseMiddleware<ErrorMiddleware>();
-            app.UseMiddleware<BrowserTypeMiddleware>();
-            app.UseMiddleware<ShortCircuitMiddleware>();
-            app.UseMiddleware<ContentMiddleware>();
+            if (env.IsDevelopment())
+            {
+                app.UseMiddleware<ErrorMiddleware>();
+                app.UseMiddleware<BrowserTypeMiddleware>();
+                app.UseMiddleware<ShortCircuitMiddleware>();
+                app.UseMiddleware<ContentMiddleware>();
+            }
+
+            app.UseMvc(routes => routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"));
         }
     }
 }
